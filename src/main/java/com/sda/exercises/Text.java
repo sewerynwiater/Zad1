dcc1edb7c8d1af62c03b1a2098cc6cd05dd05bb7
Seweryn.Wiater <swiater@tlen.pl>
2019-06-16 14:27:32 +0200

    zadanie2

---
+++ src/main/java/com/sda/exercises/Text.java
@@ -0,0 +1,8 @@
+package com.sda.exercises;
+
+public class Text {
+    public static String getText() {
+        return "Hello word";
+    }
+
+}
--- src/main/java/com/sda/exercises/Zadanie1.java
+++ src/main/java/com/sda/exercises/Zadanie1.java
@@ -2,7 +2,8 @@ package com.sda.exercises;
 
 public class Zadanie1 {
     public static void main(String[] args) {
-        System.out.println("Hello world !!!");
+        System.out.println(Text.getText());
+        System.out.println("Kolejna wersja");
     }
 
 }
